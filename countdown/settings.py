c808246b9603717e57a9f8ea670842f825222ba3
oigre <sergiomarreromarrero@gmail.com>
2025-02-16 13:47:00 +0000

    restructuration

--- countdown/settings.py
+++ countdown/settings.py
@@ -123,8 +123,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'home/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Para producción en Railway:
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
